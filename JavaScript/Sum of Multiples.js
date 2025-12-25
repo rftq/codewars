abd5b862e7ab5989751d5f2fb3335b9f7c6d877e
rftq <burdaet@gmail.com>
2025-12-25 12:30:45 +0300

    add solution*

---
+++ JavaScript/Sum of Multiples.js
@@ -0,0 +1,79 @@
+// Your Job
+
+// Find the sum of all multiples of n below m
+// Keep in Mind
+
+//     n and m are natural numbers (positive integers)
+//     m is excluded from the multiples
+
+// Examples
+
+// sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7)  ==> "INVALID"
+
+
+
+
+
+function sumMul(n,m){
+//your idea here
+let sum = 0;
+for (i=n; i < m; i+= n) {
+    sum += i;
+}
+if (n <= 0 || m <= 0) {
+    return "INVALID";
+}
+return sum;
+}
+
+console.log(sumMul(2, 9));
+
+
+/*
+function sumMul(n,m){
+  if (n >= m) return "INVALID";
+
+var sum = 0;
+  for (var i = n; i < m; i+=n) {
+    sum += i;
+  }
+  return sum;
+}
+
+
+function sumMul(n,m){
+  if (n <= 0 || m <= 0) return "INVALID";
+  
+  const last = Math.ceil(m/n) * n - n;
+  return (last + n) * (last / n) / 2;
+}
+
+
+
+function sumMul(n,m){
+if(m <= 0 || n <= 0 ) {return "INVALID";}
+let res = 0;
+for(let i = 0; i < m; i+= n){
+res += i;}
+return res;
+}
+
+
+
+
+
+const sumMul = (n, m) =>
+  m > n ? [...Array(m / n ^ 0)].map((_, idx) => ++idx * n).reduce((pre, val) => pre + val * (val < m)) : `INVALID`;
+
+
+
+
+
+
+
+
+
+*/
